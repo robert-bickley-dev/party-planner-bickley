e1e6f3b29a4d3e7142a219afd00014dbb269a531
Rob Bickley <robert.john.bickley@gmail.com>
2026-05-08 11:42:55 -0400

    Initial Pseudocode Sketch

---
+++ index.js
@@ -0,0 +1,33 @@
+// === State ===
+
+// State variables
+//      Party List [] containing Party shaped objects
+
+// Fetch functions for the API
+
+// State functions
+//      Get single party (w/render)
+//      Get all parties (w/render)
+//      (Optional) Get guests
+//      (Optional) Sort guests
+
+// === Components ===
+
+// Party list component
+//      Click listener event updates state by fetching single party info from API
+//      (Optional) CSS Styling for selected party
+
+// Party details component
+//      Name & ID, date, location, description, (Optional) guests
+//      Message if none selected (guard)
+//
+
+// === Render ===
+
+// Header
+// Party list
+// Party details
+
+// Pass through state variables
+
+// Render
